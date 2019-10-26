50ae84badfd9df7ef076b34e90057998430a0ebf
Akshay Suresh Bhat <akshaybhat2012@gmail.com>
2019-10-26 21:31:45 +0530

    Generics and Collections. Initial Commit.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Pro_Java
 Oracle Certified Professional Java SE 8 Programmer course code.
+
+## Main coverage:
+
+	* Chapter 4 : Generics and Collections.
+	* Chapter 5 : Lambda Built-in Functional Interfaces.
+	* Chapter 11 : Java Concurency(Threads).
---
+++ src/generics_collections/BoxPrinterTest.java
@@ -0,0 +1,19 @@
+// This program shows container implementation using generics
+class BoxPrinter<T> {
+	private T val;
+	public BoxPrinter(T arg) {
+		val = arg;
+	}
+	public String toString() {
+		return "[" + val + "]";
+	}
+}
+
+class BoxPrinterTest {
+	public static void main(String []args) {
+		BoxPrinter<Integer> value1 = new BoxPrinter<Integer>(new Integer(10));
+		System.out.println(value1);
+		BoxPrinter<String> value2 = new BoxPrinter<String>("Hello world");
+		System.out.println(value2);
+	}
+}
